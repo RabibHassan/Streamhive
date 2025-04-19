3bddb4f75e03a3d2df23a04fa2cd3f9575f0f042
RabibHassan <155127870+RabibHassan@users.noreply.github.com>
2025-04-20 00:51:17 +0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\WatchlistController;
 
 /*
 |--------------------------------------------------------------------------
@@ -26,10 +27,23 @@ Route::get('/login',function(){
     return view('login');
 })->name('login'); //using this cos we are using route helper instead of directly giving url in form like action="/login"
 
+Route::get('/watchlist',function(){
+    return view('watchlist');
+})->name('watchlist'); 
+
 Route::get('/UI',function(){
     return view('UI');
 })->name('UI');
 
+Route::get('/subscription',function(){
+    return view('subscription');
+})->name('subscription');
+
 Route::post('/login',[UserController::class,'login']);
 
 Route::post('/signup',[UserController::class,'signup']);
+
+Route::post('/addwatchlist',[WatchlistController::class,'addwatchlist'])->name('addwatchlist');
+
+Route::get('/watchlist', [WatchlistController::class, 'fetchmovies'])->name('watchlist'); //pt1
+Route::get('/fetchmovies',[WatchlistController::class,'fetchmovies'])->name('fetchmovies');//pt2
