1e96a72673199bdf3208657ac16120fa0f5c0e31
RabibHassan <155127870+RabibHassan@users.noreply.github.com>
2025-04-20 01:49:59 +0600

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -34,5 +34,12 @@ class UserController extends Controller
             return back()->withErrors(['email'=>'Invalid Credentials'])->onlyInput('email');
         }
     }
+
+    public function logout(Request $request){
+        auth()->logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect('/login');
+    }
 }
 
